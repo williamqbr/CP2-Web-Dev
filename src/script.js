321f1d1bed410afa5ab942b163256d15464797d9
William Queiroz <williamq2006@gmail.com>
2025-05-12 19:18:04 -0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,92 @@
+var nome = prompt('Digite seu nome:');
+var sobrenome = prompt('Digite seu sobrenome:');
+
+while (nome == '' || sobrenome == '') {
+    alert('Nome e sobrenome obrigatório');
+    var nome = prompt('Digite seu nome:');
+    var sobrenome = prompt('Digite seu sobrenome:');
+}
+
+alert("Cadasto completo! Acesse o console para mais detalhes.");
+
+console.log("Nome do usuário: " + nome);
+console.log("Sobrenome do usuário: " + sobrenome);
+
+alert('Olá, ' + nome + " " + sobrenome + '! Agora vamos aos vinhos...');
+
+var nomeVinho = prompt('Qual o nome do vinho?');
+
+while (nomeVinho == '') {
+    alert("O nome do vinho é obrigatório");
+    var nomeVinho = prompt('Qual o nome do vinho?');
+}
+
+var tipoVinho = prompt('Qual o tipo do vinho? (Tinto, Branco ou Rosé)');
+
+while (tipoVinho == '') {
+    tipoVinho = prompt("Por favor, digite o tipo do vinho:");
+}
+
+tipoVinho = tipoVinho.toLowerCase(); 
+
+if (tipoVinho == "tinto") {
+    alert("Vinho Tinto");
+} 
+else if (tipoVinho == "branco") {
+    alert("Vinho Branco");
+} 
+else if (tipoVinho == "rosé" || tipoVinho == "rose") { 
+    alert("Vinho Rosé");
+} 
+while (tipoVinho == '') {
+    alert("Tipo inválido! Só aceitamos: Tinto, Branco ou Rosé... Tente de novo.");
+    var tipoVinho = prompt('Por favor, digite o tipo do vinho:');
+}
+
+var safraVinho = prompt('Qual o ano do vinho?'); 
+
+  while (safraVinho == '') {
+    safraVinho = prompt("Digite um ano válido para a safra:");
+  }
+  safraVinho = parseInt(safraVinho);
+
+  var classificacao;                                      
+  if (safraVinho >= 2020) {
+    classificacao = "Vinho jovem";
+  } 
+  else if (safraVinho >= 2015) {
+    classificacao = "Vinho amadurecido";
+  } 
+  else {
+    classificacao = "Vinho antigo";
+  }
+
+  alert("Classificação do vinho: " + classificacao);
+
+
+  var quantidadeVinho = prompt('Qual sua quantidade no estoque?');
+
+  while (quantidadeVinho == '') {
+    quantidadeVinho = prompt("Digite uma quantidade válida:");
+  }
+  quantidadeVinho = parseInt(quantidadeVinho);
+
+  var estoque = "Estoque adequado";
+  if (quantidadeVinho <= 5) {
+    estoque = "Estoque BAIXO";
+  }
+
+  alert("Situação do estoque: " + estoque);
+
+  continuar = confirm("Deseja inserir outro vinho?");
+
+
+alert("Cadastro realizado! Veja os detalhes no console.");
+
+console.log("Usuário: " + nome + " " + sobrenome);
+console.log("Nome do vinho: " + nomeVinho);
+console.log("Tipo do vinho: " + tipoVinho);
+console.log("Sua Safra: " + safraVinho);
+console.log("Classificação do vinho: " + classificacao);
+console.log("Quantidade no estoque: " + quantidadeVinho);
+console.log("Situação que estoque se encontra: " + estoque);
